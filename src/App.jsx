8647b3c870b0a51c99f3c33de2455ba7010bf9c6
Rajan Ravisaheb <rajanroy220@gmail.com>
2024-02-06 17:12:44 +0530

    main files for react and vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import JoiForm from "./assets/JoiForm";
+import Register from "./assets/Register";
+import ZodForm from "./assets/ZodForm";
+
+function App() {
+  return (
+    <>
+      <JoiForm />
+    </>
+  );
+}
+
+export default App;
